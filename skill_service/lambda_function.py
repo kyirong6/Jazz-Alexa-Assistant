543fbc1998ea441ab2de3ccbf59fefa0fb62f071
Choenden <kyirong6@gmail.com>
2018-02-28 10:30:30 -0800

    got rid of the card for the response

--- skill_service/lambda_function.py
+++ skill_service/lambda_function.py
@@ -15,11 +15,6 @@ def build_speechlet_response(title, output, reprompt_text, should_end_session):
             'type': 'PlainText',
             'text': output
         },
-        'card': {
-            'type': 'Simple',
-            'title': "SessionSpeechlet - " + title,
-            'content': "SessionSpeechlet - " + output
-        },
         'reprompt': {
             'outputSpeech': {
                 'type': 'PlainText',
